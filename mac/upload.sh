13e48de9205772625060ec45ca1832cd0b9f763a
Christian Moritz <chrmoritz@gmail.com>
2014-04-20 17:15:07 +0200

    sdds

--- mac/upload.sh
+++ mac/upload.sh
@@ -1,3 +1,3 @@
 cd /usr
 sudo tar czf local.tar.gz local
-sudo curl -H "Authorization: token $GITHUB_KEY" -H "Accept: application/json" -H "Content-Type: application/gzip" --data-binary @/usr/local.tar.gz https://uploads.github.com/repos/chrmoritz/starcheat/releases/165510/assets?name=local.tar.gz
+sudo curl -H "Authorization: token 62f3855fb24437c11ff7ee40a8bffe8d8258fc7e" -H "Accept: application/json" -H "Content-Type: application/gzip" --data-binary @/usr/local.tar.gz https://uploads.github.com/repos/chrmoritz/starcheat/releases/165510/assets?name=local.tar.gz

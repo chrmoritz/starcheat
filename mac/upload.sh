a58e25cd8308d96c0e759f1071575305a25bda30
Christian Moritz <chrmoritz@gmail.com>
2014-12-23 01:53:05 +0100

    create cache

--- mac/upload.sh
+++ mac/upload.sh
@@ -1,3 +1,3 @@
 cd /usr
 sudo tar czf local.tar.gz local
-sudo curl -H "Authorization: token 62f3855fb24437c11ff7ee40a8bffe8d8258fc7e" -H "Accept: application/json" -H "Content-Type: application/gzip" --data-binary @/usr/local.tar.gz https://uploads.github.com/repos/chrmoritz/starcheat/releases/165510/assets?name=local.tar.gz
+sudo curl -H "Authorization: token 58a25c232ebae772f9a98607e8491ec6db42eeae" -H "Accept: application/json" -H "Content-Type: application/gzip" --data-binary @/usr/local.tar.gz https://uploads.github.com/repos/chrmoritz/starcheat/releases/165510/assets?name=local_NEW42.tar.gz
